cee3ca8513c49e4c9a373cf33ba477eecd23558e
Diogo Henrique Barbosa <contato.diogohbarbosa@gmail.com>
2022-09-27 17:07:46 -0300

    Ajust de cod. e criação de func

--- init-basic-config.sh
+++ init-basic-config.sh
@@ -1,18 +1,44 @@
 #!/usr/bin/env bash
-sudo apt-get update -y 
-sudo apt-get upgrade -y
-sudo apt-get install net-tools nmap mc -y
-clear
-echo "host  all        all        all        md5" >> /opt/bitnami/postgresql/conf/pg_hba.conf
-echo "listen_addresses = '*'" >> /opt/bitnami/postgresql/conf/postgresql.conf
-echo "Conf. os arquivos"
-sleep 5
-clear
-sudo ufw allow 5432
-service bitnami restart postgresql
-clear
 
-echo "Vai ficar nesta tela por 60 segundos!"
-cat $HOME/bitnami_credentials
-sleep 60
-exit
+function attEinstallDep(){
+  sudo apt-get update -y 
+  sudo apt-get upgrade -y
+  sudo apt-get install net-tools nmap mc -y
+  clear
+}
+
+function confArquivos(){
+  echo "Conf. os arquivos"
+  echo "host  all        all        all        md5" >> /opt/bitnami/postgresql/conf/pg_hba.conf
+  echo "listen_addresses = '*'" >> /opt/bitnami/postgresql/conf/postgresql.conf
+  sleep 5
+  clear
+}
+
+function habilitandoFirewall(){
+  echo "Habilitando Firewall"
+  sudo ufw allow 5432
+  sleep 5
+}
+
+function restServidor(){
+  echo "Rest. Servidor"
+  service bitnami restart postgresql
+  clear
+}
+function credentials(){
+  echo "Vai ficar nesta tela por 60 segundos!"
+  cat home/bitnami/bitnami_credentials
+  sleep 60
+}
+
+function main(){
+  attEinstallDep()
+  confArquivos()
+  habilitandoFirewall()
+  restServidor()
+  credentials()
+  exit
+}
+
+main()
