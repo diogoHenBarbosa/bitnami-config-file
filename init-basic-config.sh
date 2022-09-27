1e2a1f4fdf3931589e1d1482d69680c73a79ef6d
Diogo Henrique Barbosa <contato.diogohbarbosa@gmail.com>
2022-09-27 16:57:34 -0300

    Ajust e finalizando as conf

--- init-basic-config.sh
+++ init-basic-config.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-ufw allow 5432
-sudo apt-get update -y
+sudo apt-get update -y 
 sudo apt-get upgrade -y
 sudo apt-get install net-tools nmap mc -y
-
+clear
 echo "host  all        all        all        md5" >> /opt/bitnami/postgresql/conf/pg_hba.conf
-
 echo "listen_addresses = '*'" >> /opt/bitnami/postgresql/conf/postgresql.conf
-
+echo "Conf. os arquivos"
+sleep 5
+clear
+sudo ufw allow 5432
 service bitnami restart postgresql
+clear
+
+echo "Vai ficar nesta tela por 60 segundos!"
+cat $HOME/bitnami_credentials
+sleep 60
+exit
