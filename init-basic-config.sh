92e5a8895db41e96f4bf61c95b29ae29e8bb7365
Diogo Henrique Barbosa <contato.diogohbarbosa@gmail.com>
2022-09-27 16:23:26 -0300

    Correção de erros

--- init-basic-config.sh
+++ init-basic-config.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 sudo su
 ufw allow 5432
-apt-get update -Y
-apt-get upgrade -Y
-apt-get install net-tools nmap mc -Y
+apt-get update -y
+apt-get upgrade -y
+apt-get install net-tools nmap mc -y
 
 echo "host  all        all        all        md5" >> /opt/bitnami/postgresql/conf/pg_hba.conf
 
