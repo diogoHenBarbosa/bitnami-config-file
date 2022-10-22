62772c03098548bc1b44deac8311e58e4e0ed975
Diogo Henrique Barbosa <contato.diogohbarbosa@gmail.com>
2022-10-22 16:31:49 -0300

    att teste.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 function defineSenhaPostgres(){
-    export PASSWORD_POSTGRESQL= 'grep "\'************\'" ./bitnami_credentials | cut -b 54-65"
+    PASSWORD_POSTGRESQL= grep \'*\' $HOME/bitnami_credentials | cut -b 54-65
+    echo $PASSWORD_POSTGRESQL
 }
+
+defineSenhaPostgres
